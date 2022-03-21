281d199ce0ed233eb19c2d346d8c014e6c06dc24
RybiRoid <legenservis@tut.by>
2022-03-21 22:30:20 +0300

    second commit

--- src/types/commentsTypes.ts
+++ src/types/commentsTypes.ts
@@ -1,7 +1,7 @@
 export interface CommentsState {
-  comments: any[];
-  loading: boolean;
-  error: null | string;
+  comments: any[]
+  loading: boolean
+  error: null | string
 }
 export enum CommentsActionTypes {
   FETCH_COMMENTS = 'FETCH_COMMENTS',
@@ -9,17 +9,17 @@ export enum CommentsActionTypes {
   FETCH_COMMENTS_ERROR = 'FETCH_COMMENTS_ERROR',
 }
 interface FetchCommentsAction {
-  type: CommentsActionTypes.FETCH_COMMENTS;
+  type: CommentsActionTypes.FETCH_COMMENTS
 }
 interface FetchCommentsSuccessAction {
-  type: CommentsActionTypes.FETCH_COMMENTS_SUCCESS;
-  payload: any[];
+  type: CommentsActionTypes.FETCH_COMMENTS_SUCCESS
+  payload: any[]
 }
 interface FetchCommentsErrorAction {
-  type: CommentsActionTypes.FETCH_COMMENTS_ERROR;
-  payload: string;
+  type: CommentsActionTypes.FETCH_COMMENTS_ERROR
+  payload: string
 }
 export type CommentsAction =
   | FetchCommentsAction
   | FetchCommentsSuccessAction
-  | FetchCommentsErrorAction;
+  | FetchCommentsErrorAction
